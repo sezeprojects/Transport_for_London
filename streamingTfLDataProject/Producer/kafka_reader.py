a86381523c3986c6535997a8cd48b2a9432a132e
Ezenwa Ugbomah <eze.ugbomah@gmail.com>
2023-06-12 13:44:33 +0100

    added crowding data to kafak cluster, successfully read both data into spark streaming

--- streamingTfLDataProject/Producer/kafka_reader.py
+++ streamingTfLDataProject/Producer/kafka_reader.py
@@ -1,19 +1,31 @@
 from pyspark.sql import SparkSession
+import os
 
-topic = 'TfL-bus-arrival'
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell'
 
 # Create a SparkSession
 spark = SparkSession.builder \
     .appName("KafkaReader") \
     .getOrCreate()
 
-# Subscribe to 1 topic
-df = spark \
+# Subscribe to bus-arrival topic
+bus_arrival_df = spark \
   .read \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", topic) \
+  .option("subscribe", 'TfL-bus-arrival') \
   .load()
-# df.isStreaming()
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-df.show()
+# bus_arrival_df.isStreaming()
+bus_arrival_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+bus_arrival_df.show()
+
+# Subscribe to crowding topic
+crowding_df = spark \
+  .read \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "localhost:9092") \
+  .option("subscribe", 'TfL-crowding') \
+  .load()
+# crowding_df.isStreaming()
+crowding_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+crowding_df.show()
--- streamingTfLDataProject/Producer/stream_producer.py
+++ streamingTfLDataProject/Producer/stream_producer.py
@@ -1,4 +1,5 @@
 from streamingTfLDataProject.Producer.pyspark_producer import stream_data
+import threading
 
 
 # bus_arrivals
@@ -6,7 +7,21 @@ bus_arrival_topic = 'TfL-bus-arrival'
 bus_arrival_url = "https://api.tfl.gov.uk/Stoppoint?lat=51.4929&lon=" \
       "0.053929&stoptypes=NaptanBusCoachStation," \
       "NaptanPublicBusCoachTram"
-stream_data(bus_arrival_topic, bus_arrival_url)
+# stream_data(bus_arrival_topic, bus_arrival_url)
 
 #crowding
-crowing_topic = 'TfL-crowding'
+crowding_topic = 'TfL-crowding'
+crowding_url = 'https://api.tfl.gov.uk/crowding/940GZZLUBND'
+
+#
+
+# Create and start threads for each invocation
+thread_bus_arrival = threading.Thread(target=stream_data, args=(bus_arrival_topic, bus_arrival_url))
+thread_crowding = threading.Thread(target=stream_data, args=(crowding_topic, crowding_url))
+
+thread_bus_arrival.start()
+thread_crowding.start()
+
+# Wait for both threads to complete
+thread_bus_arrival.join()
+thread_crowding.join()
