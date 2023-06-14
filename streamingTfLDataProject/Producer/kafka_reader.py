99730fcca7cccb0c3b435db34b998f2281f9a4bb
Ezenwa Ugbomah <eze.ugbomah@gmail.com>
2023-06-14 22:08:02 +0100

    flattened crowding data into sub data and subtracted timebounds data into another dataframe

--- streamingTfLDataProject/Producer/kafka_reader.py
+++ streamingTfLDataProject/Producer/kafka_reader.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, ArrayType, FloatType
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, explode
 import os
 
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell'
@@ -50,18 +50,21 @@ json_df = json_df.withColumn("daysOfWeek", col("json_struct.daysOfWeek"))
 json_df.printSchema()
 json_df.show()
 
-crowding_df = json_df.select("daysOfWeek")
-crowding_df.printSchema()
-crowding_df.show()
+days_of_week = json_df.select("daysOfWeek")
+days_of_week.printSchema()
+days_of_week.show()
 
-# # Accessing values from 'stopPoints_value' array
-crowding_df = crowding_df.select(col("daysOfWeek.dayOfWeek").alias("dayOfWeek"),
+# Accessing values from 'crowding_value' array
+crowding_df = days_of_week.select(col("daysOfWeek.dayOfWeek").alias("dayOfWeek"),
                    col("daysOfWeek.amPeakTimeBand").alias("amPeakTimeBand"),
                    col("daysOfWeek.pmPeakTimeBand").alias("pmPeakTimeBand"),
                    col("daysOfWeek.timeBands").alias("timeBands")
                   )
 crowding_df.show()
 
-# timeBands_df = crowding_df.select(col('timeBands.percentageOfBaseLine').alias('percentageOfBaseLine'))
-# timeBands_df.show()
+time_band_df = crowding_df.select(explode("timeBands").alias("timeBand_data"))
 
+# Accessing the nested fields
+time_band_df = time_band_df.select("timeBand_data.timeBand", "timeBand_data.percentageOfBaseLine")
+
+time_band_df.show()
