3fa0fec82a2fe12979951c7092541dd9e9c4cdbd
Ezenwa Ugbomah <eze.ugbomah@gmail.com>
2023-06-14 22:36:10 +0100

    added valid_severity_url to stream_producer

--- streamingTfLDataProject/Producer/stream_producer.py
+++ streamingTfLDataProject/Producer/stream_producer.py
@@ -14,14 +14,19 @@ crowding_topic = 'TfL-crowding'
 crowding_url = 'https://api.tfl.gov.uk/crowding/940GZZLUBND'
 
 #
+valid_severity_topic = 'TfL-severity'
+valid_severity_url = 'https://api.tfl.gov.uk/Line/Meta/Severity'
 
 # Create and start threads for each invocation
 thread_bus_arrival = threading.Thread(target=stream_data, args=(bus_arrival_topic, bus_arrival_url))
 thread_crowding = threading.Thread(target=stream_data, args=(crowding_topic, crowding_url))
+valid_severity = threading.Thread(target=stream_data, args=(valid_severity_topic, valid_severity_url))
 
 thread_bus_arrival.start()
 thread_crowding.start()
+valid_severity.start()
 
 # Wait for both threads to complete
 thread_bus_arrival.join()
 thread_crowding.join()
+valid_severity.join()
