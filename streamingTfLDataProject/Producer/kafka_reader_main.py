4a4326c99a59b6c31ba32e1d92c1c47d91e0b65f
Sesan Olaiya <nase2see@gmail.com>
2023-06-29 12:56:38 +0100

    reassigned appropriate variables and defined schema for arrival time

--- streamingTfLDataProject/Producer/kafka_reader_main.py
+++ streamingTfLDataProject/Producer/kafka_reader_main.py
@@ -1,9 +1,13 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, FloatType, BooleanType, DoubleType
+from pyspark.sql.types import IntegerType, StructType, StructField, StringType, ArrayType, FloatType, \
+    BooleanType, \
+    DoubleType
 from pyspark.sql.functions import from_json, col, explode
 import os
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell'
+os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages org.apache.spark:" \
+                                    "spark-streaming-kafka-0-10_2.12:3.2.0," \
+                                    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 pyspark-shell"
 
 # Create a SparkSession
 spark = SparkSession.builder \
@@ -15,65 +19,65 @@ df = spark \
   .read \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "localhost:9092") \
-  .option("subscribe", 'TfL-bus-arrival') \
+  .option("subscribe", 'TfL-stop-points') \
   .load()
 
-df.printSchema()
-df.show()
+# df.printSchema()
+# df.show()
 
 # convert the json into string from binary to make it accessible
 json_df = df.select("value")
 json_df = json_df.withColumn("json_string", json_df["value"].cast("string"))
 
 json_schema = StructType([
-                          StructField('$type', StringType(), nullable=False),
-                          StructField('centrePoint', StringType(), nullable=False),
+                          StructField('$type', StringType(), nullable=True),
+                          StructField('centrePoint', StringType(), nullable=True),
                           StructField('stopPoints', ArrayType(
                               StructType([
-                                  StructField('$type', StringType(), nullable=False),
-                                  StructField('naptanId', StringType(), nullable=False),
-                                  StructField('indicator', StringType(), nullable=False),
-                                  StructField('stopLetter', StringType(), nullable=False),
-                                  StructField('modes', ArrayType(StringType()), nullable=False),
-                                  StructField('icsCode', StringType(), nullable=False),
-                                  StructField('stopType', StringType(), nullable=False),
-                                  StructField('stationNaptan', StringType(), nullable=False),
+                                  StructField('$type', StringType(), nullable=True),
+                                  StructField('naptanId', StringType(), nullable=True),
+                                  StructField('indicator', StringType(), nullable=True),
+                                  StructField('stopLetter', StringType(), nullable=True),
+                                  StructField('modes', ArrayType(StringType()), nullable=True),
+                                  StructField('icsCode', StringType(), nullable=True),
+                                  StructField('stopType', StringType(), nullable=True),
+                                  StructField('stationNaptan', StringType(), nullable=True),
                                   StructField('lines', ArrayType(
                                       StructType([
-                                          StructField('$type', StringType(), nullable=False),
-                                          StructField('id', StringType(), nullable=False),
-                                          StructField('name', StringType(), nullable=False),
-                                          StructField('uri', StringType(), nullable=False),
-                                          StructField('type', StringType(), nullable=False),
+                                          StructField('$type', StringType(), nullable=True),
+                                          StructField('id', StringType(), nullable=True),
+                                          StructField('name', StringType(), nullable=True),
+                                          StructField('uri', StringType(), nullable=True),
+                                          StructField('type', StringType(), nullable=True),
                                           StructField('crowding', StructType([
-                                              StructField('$type', StringType(), nullable=False)
-                                          ]), nullable=False),
-                                          StructField('routeType', StringType(), nullable=False),
-                                          StructField('status', StringType(), nullable=False)
+                                              StructField('$type', StringType(), nullable=True)
+                                          ]), nullable=True),
+                                          StructField('routeType', StringType(), nullable=True),
+                                          StructField('status', StringType(), nullable=True)
                                       ])
-                                  ), nullable=False),
+                                  ), nullable=True),
                                   StructField('lineGroup', ArrayType(
                                       StructType([
-                                          StructField('$type', StringType(), nullable=False),
-                                          StructField('naptanIdReference', StringType(), nullable=False),
-                                          StructField('stationAtcoCode', StringType(), nullable=False),
+                                          StructField('$type', StringType(), nullable=True),
+                                          StructField('naptanIdReference', StringType(), nullable=True),
+                                          StructField('stationAtcoCode', StringType(), nullable=True),
                                           StructField('lineIdentifier', ArrayType(
-                                              StringType()), nullable=False)
+                                              StringType()), nullable=True)
                                       ])
-                                  ), nullable=False),
+                                  ), nullable=True),
                                   StructField('lineModeGroups', ArrayType(
                                       StructType([
-                                          StructField('$type', StringType(), nullable=False),
-                                          StructField('modeName', StringType(), nullable=False),
+                                          StructField('$type', StringType(), nullable=True),
+                                          StructField('modeName', StringType(), nullable=True),
                                           StructField('lineIdentifier', ArrayType(
-                                              StringType()), nullable=False)
+                                              StringType()), nullable=True)
                                       ])
-                                  ), nullable=False),
-                                  StructField('status', BooleanType(), nullable=False),
-                                  StructField('id', StringType(), nullable=False),
-                                  StructField('commonName', StringType(), nullable=False),
-                                  StructField('distance', StringType(), nullable=False),
-                                  StructField('placeType', StringType(), nullable=False),
+                                  ), nullable=True),
+                                  StructField('status', BooleanType(), nullable=True),
+                                  StructField('id', StringType(), nullable=True),
+                                  StructField('commonName', StringType(), nullable=True),
+                                  StructField('distance', StringType(), nullable=True),
+                                  StructField('placeType', StringType(), nullable=True),
                                   StructField('additionalProperties', ArrayType(
                                       StructType([
                                           StructField('$type', StringType(), nullable=True),
@@ -83,14 +87,14 @@ json_schema = StructType([
                                           StructField('value', StringType(), nullable=True)
                                       ])
                                   ), nullable=True),
-                                  StructField('children', ArrayType(StringType()), nullable=False),
-                                  StructField('lat', DoubleType(), nullable=False),
-                                  StructField('lon', DoubleType(), nullable=False)
+                                  StructField('children', ArrayType(StringType()), nullable=True),
+                                  StructField('lat', DoubleType(), nullable=True),
+                                  StructField('lon', DoubleType(), nullable=True)
                               ])
-                          ), nullable=False),
-                          StructField('pageSize', StringType(), nullable=False),
-                          StructField('total', StringType(), nullable=False),
-                          StructField('page', StringType(), nullable=False)
+                          ), nullable=True),
+                          StructField('pageSize', StringType(), nullable=True),
+                          StructField('total', StringType(), nullable=True),
+                          StructField('page', StringType(), nullable=True)
                           ])
 
 # Parse the JSON data with the provided schema
@@ -99,18 +103,18 @@ json_df = json_df.withColumn("json_struct", from_json("json_string", json_schema
 # Extract the value of the stopPoints field
 json_df = json_df.withColumn("stopPoints_value", col("json_struct.stopPoints"))
 
-json_df.printSchema()
-json_df.show()
+# json_df.printSchema()
+# json_df.show()
 
-arrival_df = json_df.select("stopPoints_value")
-arrival_df.printSchema()
-arrival_df.show()
+stop_points_df = json_df.select("stopPoints_value")
+# arrival_df.printSchema()
+# arrival_df.show()
 
 # Accessing values from 'stopPoints_value' array
-arrival_df.select(col("stopPoints_value.$type").alias("stop_point_type"),
-                  col("stopPoints_value.naptanId").alias("stop_point_naptanId"),
-                  col("stopPoints_value.indicator").alias("stop_point_indicator")
-                  ).show()
+stop_points_df.select(col("stopPoints_value.$type").alias("stop_point_type"),
+                      col("stopPoints_value.naptanId").alias("stop_point_naptanId"),
+                      col("stopPoints_value.indicator").alias("stop_point_indicator")
+                      )  # .show()
 
 df = spark.read \
   .format("kafka") \
@@ -118,14 +122,12 @@ df = spark.read \
   .option("subscribe", 'TfL-crowding') \
   .load()
 
-df.printSchema()
-df.show()
+# df.printSchema()
+# df.show()
 
 # convert the json into string from binary to make it accessible
 json_df = df.select("value")
 json_df = json_df.withColumn("json_string", json_df["value"].cast("string"))
-
-
 
 json_schema = StructType([
                           StructField('naptan', StringType(), nullable=True),
@@ -149,24 +151,90 @@ json_df = json_df.withColumn("json_struct", from_json("json_string", json_schema
 # Extract the value of the stopPoints field
 json_df = json_df.withColumn("daysOfWeek", col("json_struct.daysOfWeek"))
 
-json_df.printSchema()
-json_df.show()
+# json_df.printSchema()
+# json_df.show()
 
 days_of_week = json_df.select("daysOfWeek")
-days_of_week.printSchema()
-days_of_week.show()
+# days_of_week.printSchema()
+# days_of_week.show()
 
 # Accessing values from 'crowding_value' array
 crowding_df = days_of_week.select(col("daysOfWeek.dayOfWeek").alias("dayOfWeek"),
-                   col("daysOfWeek.amPeakTimeBand").alias("amPeakTimeBand"),
-                   col("daysOfWeek.pmPeakTimeBand").alias("pmPeakTimeBand"),
-                   col("daysOfWeek.timeBands").alias("timeBands")
-                  )
-crowding_df.show()
+                                  col("daysOfWeek.amPeakTimeBand").alias("amPeakTimeBand"),
+                                  col("daysOfWeek.pmPeakTimeBand").alias("pmPeakTimeBand"),
+                                  col("daysOfWeek.timeBands").alias("timeBands")
+                                  )
+# crowding_df.show()
 
 time_band_df = crowding_df.select(explode("timeBands").alias("timeBand_data"))
 
 # Accessing the nested fields
 time_band_df = time_band_df.select("timeBand_data.timeBand", "timeBand_data.percentageOfBaseLine")
 
-time_band_df.show()
+# time_band_df.show()
+
+df = spark.read \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", "localhost:9092") \
+  .option("subscribe", 'stop-DE') \
+  .load()
+
+# df.printSchema()
+# df.show()
+
+# convert the json into string from binary to make it accessible
+json_df = df.select("value")
+json_df = json_df.withColumn("json_string", json_df["value"].cast("string"))
+
+json_schema = StructType([
+                          StructField('$type', StringType(), nullable=True),
+                          StructField('id', StringType(), nullable=True),
+                          StructField('operationType', IntegerType(), nullable=True),
+                          StructField('vehicleId', StringType(), nullable=True),
+                          StructField('naptanId', StringType(), nullable=True),
+                          StructField('stationName', StringType(), nullable=True),
+                          StructField('lineId', StringType(), nullable=True),
+                          StructField('lineName', StringType(), nullable=True),
+                          StructField('platformName', StringType(), nullable=True),
+                          StructField('direction', StringType(), nullable=True),
+                          StructField('bearing', StringType(), nullable=True),
+                          StructField('destinationNaptanId', StringType(), nullable=True),
+                          StructField('destinationName', StringType(), nullable=True),
+                          StructField('timestamp', StringType(), nullable=True),
+                          StructField('timeToStation', IntegerType(), nullable=True),
+                          StructField('currentLocation', StringType(), nullable=True),
+                          StructField('towards', StringType(), nullable=True),
+                          StructField('expectedArrival', StringType(), nullable=True),
+                          StructField('timeToLive', StringType(), nullable=True),
+                          StructField('modeName', StringType(), nullable=True),
+                          StructField('timing', StructType([
+                              StructField('$type', StringType(), nullable=True),
+                              StructField('countdownServerAdjustment', StringType(), nullable=True),
+                              StructField('source', StringType(), nullable=True),
+                              StructField('insert', StringType(), nullable=True),
+                              StructField('read', StringType(), nullable=True),
+                              StructField('sent', StringType(), nullable=True),
+                              StructField('received', StringType(), nullable=True)
+                          ]))
+                      ])
+
+# Parse the array string and extract its element as a column
+json_array_df = json_df.withColumn("json_array", from_json(col("json_string"), "array<string>"))
+
+# json_array_df.printSchema()
+# json_array_df.show()
+
+json_exploded_df = json_array_df.withColumn("exploded_array", explode(col("json_array")))
+# json_exploded_df.printSchema()
+# json_exploded_df.show()
+
+# Parse the JSON data with the provided schema
+json_struct_df = json_exploded_df.withColumn("json_struct", from_json("exploded_array", json_schema))
+json_struct_df.printSchema()
+# json_struct_df.show()
+
+json_struct_df = json_struct_df.drop("value", "json_string", "json_array", "exploded_array")
+json_struct_df.show()
+
+arrival_info_df = json_struct_df.select("json_struct.stationName")
+arrival_info_df.show()
--- streamingTfLDataProject/Producer/stream_producer.py
+++ streamingTfLDataProject/Producer/stream_producer.py
@@ -6,8 +6,8 @@ naptan_ids = ('490001345A', '490011886W', '490006983DE', '490011247DC', '4900069
 naptan_id_topics = ('stop-DA', 'stop-DD', 'stop-DE', 'stop-DC', 'stop-DG', 'stop-DB')
 
 # bus_arrivals
-bus_arrival_topic = 'TfL-bus-arrival'
-bus_arrival_url = "https://api.tfl.gov.uk/Stoppoint?lat=51.4929&lon=" \
+stop_points_topic = 'TfL-stop-points'
+stop_points_url = "https://api.tfl.gov.uk/Stoppoint?lat=51.4929&lon=" \
                   "0.053929&stoptypes=NaptanBusCoachStation," \
                   "NaptanPublicBusCoachTram"
 
@@ -20,41 +20,45 @@ valid_severity_topic = 'TfL-severity'
 valid_severity_url = 'https://api.tfl.gov.uk/Line/Meta/Severity'
 
 
-def stream_stop_time(naptan_id, naptan_id_topic):
-    bus_time_url = f"https://api.tfl.gov.uk/StopPoint/{naptan_id}/Arrivals?mode=bus"
-    bus_time = stream_data(naptan_id_topic, bus_time_url)
-    return bus_time
+def stream_arrival_time(naptan_id, naptan_id_topic):
+    arrival_url = f"https://api.tfl.gov.uk/StopPoint/{naptan_id}/Arrivals?mode=bus"
+    arrival_time = stream_data(naptan_id_topic, arrival_url)
+    return arrival_time
 
 
-def start_stream_data(topic, url):
+def start_streaming_data(topic, url):
     stream_data(topic, url)
 
 
 if __name__ == '__main__':
     # Start the remaining threads
-    threads = []
+    all_threads = []
 
     # Start bus_arrival process
-    bus_arrival_thread = threading.Thread(target=start_stream_data, args=(bus_arrival_topic, bus_arrival_url))
-    bus_arrival_thread.start()
-    threads.append(bus_arrival_thread)
+    stop_points_thread = threading.Thread(target=start_streaming_data,
+                                          args=(stop_points_topic, stop_points_url))
+    stop_points_thread.start()
+    all_threads.append(stop_points_thread)
 
     # Start crowding process
-    crowding_thread = threading.Thread(target=start_stream_data, args=(crowding_topic, crowding_url))
+    crowding_thread = threading.Thread(target=start_streaming_data,
+                                       args=(crowding_topic, crowding_url))
     crowding_thread.start()
-    threads.append(crowding_thread)
+    all_threads.append(crowding_thread)
 
     # Start valid_severity process
-    valid_severity_thread = threading.Thread(target=start_stream_data, args=(valid_severity_topic, valid_severity_url))
+    valid_severity_thread = threading.Thread(target=start_streaming_data,
+                                             args=(valid_severity_topic, valid_severity_url))
     valid_severity_thread.start()
-    threads.append(valid_severity_thread)
+    all_threads.append(valid_severity_thread)
 
     # Create and start threads for each naptan_id and naptan_topic pair
     for bus_naptan_id, nap_id_topic in zip(naptan_ids, naptan_id_topics):
-        process = threading.Thread(target=stream_stop_time, args=(bus_naptan_id, nap_id_topic))
-        process.start()
-        threads.append(process)
+        bus_time_thread = threading.Thread(target=stream_arrival_time,
+                                           args=(bus_naptan_id, nap_id_topic))
+        bus_time_thread.start()
+        all_threads.append(bus_time_thread)
 
     # Wait for all threads to complete
-    for thread in threads:
+    for thread in all_threads:
         thread.join()
